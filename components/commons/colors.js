36b91d5e6b4a754f613ce04abc98925d82dc2f45
Owais Idris <owais.i@peopleinteractive.in>
2018-05-14 15:27:31 +0530

    Colors Squash mached to standard colors from pallete

--- components/commons/colors.js
+++ components/commons/colors.js
@@ -1,19 +1,73 @@
 export const colorSquash = {
-  red1: "#FF5A60",
+  pink1: "#FF5A60",
+  pink2:"#FF8B8F",
+  pink3:"#FEC6C8",
+  pink4:"#FDEDEE",
+  pink5:"#E53A41",
+  pink6:"#F14D53",
+  red1:"#FF0000",
   red2: "##B71C1C",
-  blue1: "#2196F3",
+  blue1: "#00BCD5",
+  blue2:"#83E1ED",
+  blue3:"#DBF7FB",
+  blue4:"#0194A8",
+  blue5:"#1BA3B6",
   green1: "#4CAF50",
+  green2:"#89C965",
   white: "#FFFFFF",
   black: "#000",
-  grey1: "#ddd"
+  grey1: "#ddd",
+  grey2:"#51505D",
+  grey3:"#72727D",
+  grey4:"#95959D",
+  grey5:"#B1B3B9",
+  grey6:"#CDCED1",
+  grey7:"#DFE0E3",
+  grey8:"#F1F1F2",
+  grey9:"#F4F4F7",
+  yellow1:"#FFB400",
+  torquoise:"#01D1C1",
+  lavender:"#9375CB",
+  columbia_blue:"#85C1FF",
+  vibrant_red:"#FF585C",
+  warm_peach:"#FAA078",
+
+
+
+
 };
 
-export const color_primary = colorSquash.red1;
+export const color_primary = colorSquash.pink1;
 export const color_secondary = colorSquash.blue1;
-export const color_primary_dark = colorSquash.red2;
-export const color_divider = colorSquash.grey1;
+
+export const color_primary_dark = colorSquash.pink6;
+export const color_primary_light=colorSquash.pink2;
+export const color_primary_lighter=colorSquash.pink4;
+
+//For Accent Color buttons and components
+export const color_secondary_medium=colorSquash.blue2;
+export const color_secondary_light=colorSquash.blue3;
+export const color_hover=colorSquash.blue4
+export const color_hover_secondary=colorSquash.blue5
+
+
+
+
+export const color_text_primary=colorSquash.grey2;
+export const color_text_secondary=colorSquash.grey3;
+export const color_text_tertiary=colorSquash.grey4;
+export const color_text_hint=colorSquash.grey5;
+
+export const color_text_deactivated=colorSquash.grey6;
+export const color_divider = colorSquash.grey7;
+export const color_page_background=colorSquash.grey8;
+export const color_pending_notification=colorSquash.grey9;
+
 
 export const color_text_dark = colorSquash.white;
 export const color_text_light = colorSquash.black;
 
-export const color_offer = colorSquash.green1;
+//other colors
+export const color_offer = colorSquash.green2;
+export const color_notification_dot=colorSquash.red1;
+
